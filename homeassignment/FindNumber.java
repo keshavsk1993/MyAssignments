9f6f13fbe4a34421391ced5435f2dc90ab961612
keshavsk1993 <149048351+keshavsk1993@users.noreply.github.com>
2023-10-27 14:02:55 +0530

    Add files via upload
    week1day1

---
+++ homeassignment/FindNumber.java
@@ -0,0 +1,14 @@
+package week1.day1.homeassignment;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+
+		int number = 45;
+		if (number > 0) {
+			System.out.println("the given number is a postive number");
+		} else {
+			System.out.println("the given number is a negative number");
+		}
+	}
+}
---
+++ homeassignment/FirefoxBrowser.java
@@ -0,0 +1,20 @@
+package week1.day1.homeassignment;
+
+public class FirefoxBrowser {
+
+	public static void main(String[] args) {
+
+		float browserVersion = 100.12f;
+		String browserName = "firefox";
+		boolean pageLoaded = true;
+		int noOfBrowser = 1;
+		char browserLogo = 'f';
+		System.out.println("browserVersion is " + browserVersion);
+		System.out.println("browserName is " + browserName);
+		System.out.println("pageLoaded is " + pageLoaded);
+		System.out.println("noOfBrowser is " + noOfBrowser);
+		System.out.println("browserLogo is " + browserLogo);
+
+	}
+
+}
